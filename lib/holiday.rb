2e4546d32d4e271811e5c1081834f19a7f3a32da
emi kojima <emi8emi@gmail.com>
2018-02-22 17:08:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,13 +61,13 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    end
+   
    
     holiday.each do |holiday, supplies|
        binding.pry
     
 
-
+ end
 end
 end
 
