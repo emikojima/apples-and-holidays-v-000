67f906933c8e050791a9b16a8ad14206096204be
emi kojima <emi8emi@gmail.com>
2018-02-22 17:13:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
-       binding.pry
+      puts "#{holiday_name.capitalize}: #{supplies}"" 
+      # binding.pry
     
 
  end
